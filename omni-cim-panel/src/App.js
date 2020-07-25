bb75a3c82523149cc8b53b5d5aecfe300c4c7388
Ruan Carvalho <ruancarvalho@gmail.com>
2020-07-24 22:45:29 -0400

    feat(panel): add customer creation feature

--- omni-cim-panel/src/App.js
+++ omni-cim-panel/src/App.js
@@ -1,26 +1,136 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { Button, Form, FormGroup, Label, Input, FormFeedback } from 'reactstrap';
 
 function App() {
+  
+  const [customer, setCustomer] = useState({});
+  const [address, setAddress] = useState({});
+  const [validZip, setValidZip] = useState(undefined);
+
+  function handleChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    setCustomer({[name]: value});
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await fetch('/api/customers', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(customer),
+    });
+    const body = await response.text();
+    
+    setCustomer(emptyCustomer);
+    setAddress(emptyAddress);
+  };
+
+  function renderForm() {
+
+    return (
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label for="name">Nome</Label>
+          <Input type="text" name="name" id="name" value={customer.name}  onChange={handleChange} required />
+        </FormGroup>
+        <FormGroup>
+          <Label for="zipcode">CEP</Label>
+          {validZip === undefined 
+            ? <Input type="text" name="zipcode" id="zipcode" onBlur={checkZipcode} required />
+            : <Input type="text" name="zipcode" id="zipcode" onBlur={checkZipcode} valid={validZip} invalid={!validZip} required />
+          }
+          <FormFeedback>Formato de CEP inválido</FormFeedback>
+        </FormGroup>
+        <FormGroup>
+          <Label for="street">Logradouro</Label>
+          <Input type="text" name="street" id="street" value={address.logradouro} disabled />
+        </FormGroup>
+        <FormGroup>
+          <Label for="complement">Complemento</Label>
+          <Input type="text" name="complement" id="complement" value={address.complemento} onChange={handleChange} />
+        </FormGroup>
+        <FormGroup>
+          <Label for="hood">Bairro</Label>
+          <Input type="text" name="hood" id="hood" value={address.bairro} disabled />
+        </FormGroup>
+        <FormGroup>
+          <Label for="city">Município</Label>
+          <Input type="text" name="city" id="city" value={address.localidade} disabled />
+        </FormGroup>
+        <FormGroup>
+          <Label for="state">UF</Label>
+          <Input type="text" name="state" id="state" value={address.uf} disabled />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          YEAP
-        </a>
+    <div className="app">
+      <header className="container my-5">
+        <h1>Customer Interactions</h1>
       </header>
+      <main className="container">
+        <h2>Criar Conta</h2>
+        {renderForm()}
+      </main>
     </div>
   );
+
+  function viacepCallback(json) {
+    if ("erro" in json) {
+      setValidZip(false)
+      setAddress(emptyAddress);
+    }
+    else {      
+      setAddress(json)
+      setValidZip(true);
+    }
+  }
+
+  function checkZipcode(e) {
+    const zip = e.target.value.replace(/\D/g, '');
+    const zipRegex = /^[0-9]{8}$/;
+
+    if (zip !== "") {
+      if (zipRegex.test(zip)) {
+
+        fetch(`https://viacep.com.br/ws/${zip}/json/`)
+          .then(response => response.json())          
+          .then(data => viacepCallback(data));
+      } else {
+          setValidZip(false)
+          setAddress(emptyAddress);
+      }
+    } else {
+      setValidZip(false)
+      setAddress(emptyAddress());
+    }
+  }
+}
+
+const emptyAddress = () => {
+  return {
+    "cep": "",
+    "logradouro": "",
+    "complemento": "",
+    "bairro": "",
+    "localidade": "",
+    "uf": "",
+  }
+}
+
+const emptyCustomer = () => {
+  return {
+    "name": "",    
+  }
 }
 
 export default App;
